4e1388b9d47f90bbe032fc44991346b7764cefc0
yarinisraeli24 <74715967+yarinisraeli24@users.noreply.github.com>
2021-06-16 14:35:05 +0300

    connect users to DB

--- TerminalY/Controllers/AccountsController.cs
+++ TerminalY/Controllers/AccountsController.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TerminalY.Data;
 using TerminalY.Models;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
 
 namespace TerminalY.Controllers
 {
@@ -151,14 +156,107 @@ namespace TerminalY.Controllers
         }
 
         //added functions
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Id,Username,Password,Gender,Name,BirthDate,Registered")] Account account, string re_pass, string agree_term)
+        {
+            if (agree_term != "true")
+            {
+                TempData["TermsError"] = "You must agree to the terms";
+                return View(account);
+            }
+            var user = await _context.Account.FirstOrDefaultAsync(u => account.Username == u.Username);
+            if (user != null)
+            {
+                ModelState.AddModelError("Username", "This Email is already exist");
+                return View();
+            }
+            if (account.Password != re_pass)
+            {
+                TempData["Error"] = "The password doesn't match";
+                account.Password = null;
+                return View(account);
+            }
+            if (ModelState.IsValid)
+            {
+                //account.Cart = new Cart();
+                account.Registered = DateTime.Now;
+                account.Role = (Role)1;
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                SignIn(account);
+                return RedirectToAction(nameof(Index), "Home");
+            }
+            return View(account);
+        }
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult Register()
-        {
-            return View();
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string Username, string Password, string Remember_me)
+        {
+            var account = await _context.Account.FirstOrDefaultAsync(u => Username == u.Username);
+            if (account == null)
+            {
+                ModelState.AddModelError("Username", "This Email doesn't exist");
+                return View();
+            }
+            account = await _context.Account.FirstOrDefaultAsync(u => (Username == u.Username) && (Password == u.Password));
+            if (account == null)
+            {
+                ModelState.AddModelError("Password", "Wrong Password");
+                return View();
+            }
+            SignIn(account, Remember_me);
+            return RedirectToAction(nameof(Index), "Home");
         }
+
+        private async void SignIn(Account user, string Remember_me = "false")
+        {
+            var claims = new List<Claim>
+            {
+
+                new Claim(ClaimTypes.Email, user.Username),
+                new Claim("Name", user.Name),
+                new Claim(ClaimTypes.Role, user.Role.ToString()),
+            };
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+            if (Remember_me == "false")
+            {
+                authProperties.ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10);
+            }
+            else
+            {
+                authProperties.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(3);
+            }
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+        private bool AccountsExists(int id)
+        {
+            return _context.Account.Any(e => e.Id == id);
+        }
+
     }
 }
